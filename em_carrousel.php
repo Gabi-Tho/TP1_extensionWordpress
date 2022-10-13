fdd4349484d78fcc9689c0a62a06332f110e89e7
Gabi-Tho <102246449+Gabi-Tho@users.noreply.github.com>
2022-10-13 11:50:23 -0400

    creation de function emc_enqueue

--- em_carrousel.php
+++ em_carrousel.php
@@ -6,6 +6,39 @@
 /*Plugin Name: gt_carrousel
 */
 
+
+/*
+filemtime()  // retourne en milliseconde le temps de la dernière sauvegarde
+plugin_dir_path() // retourne le chemin du répertoire du plugin
+__FILE__ // le fichier en train de s'exécuter
+wp_enqueue_style() // Intègre le link:css dans la page
+wp_enqueue_script() // intègre le script dans la page
+wp_enqueue_scripts // le hook
+*/
+
+function emc_enqueue()
+{
+    $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+
+    wp_enqueue_style("emc_style_carrousel",
+
+                    plugin_dir_url(__FILE__) . "style.css",
+                    array(),
+                    $version_css,
+                    false);
+
+
+    wp_enqueue_script("emc_js_carrousel",
+
+                    plugin_dir_url(__FILE__) . "js/carrousel.js",
+                    array(),
+                    $version_js,
+                    false);
+}
+
+add_action();
+
 function genere_boite(){
 
     $contenu = "
